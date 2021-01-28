d736e324a9426c2b01b464ae2f9376a322fd3009
griezma <griezma@outlook.com>
2021-01-28 17:27:41 +0100

    password encryption, config profiles refactored

--- src/main/java/griezma/mssc/beerservice/BeerServiceApplication.java
+++ src/main/java/griezma/mssc/beerservice/BeerServiceApplication.java
@@ -1,10 +1,12 @@
 package griezma.mssc.beerservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
@@ -12,4 +14,12 @@ public class BeerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BeerServiceApplication.class, args);
     }
+
+//    @Bean
+//    ApplicationRunner runner(Environment env) {
+//        return args -> {
+//            log.info("Using database " + env.getProperty("spring.jpa.database"));
+//            log.info("Config source is " + env.getProperty("beerworks.configsource.id"));
+//        };
+//    }
 }
